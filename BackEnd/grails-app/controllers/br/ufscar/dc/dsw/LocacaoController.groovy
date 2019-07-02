949185070d0b3b3c8a6afd2afd03fc1abd746b78
Gabriel Nardy <gtnardy@gmail.com>
2019-07-02 11:03:47 -0300

    configuracao dos ROLES em cada controller

--- BackEnd/grails-app/controllers/br/ufscar/dc/dsw/LocacaoController.groovy
+++ BackEnd/grails-app/controllers/br/ufscar/dc/dsw/LocacaoController.groovy
@@ -3,7 +3,9 @@ package br.ufscar.dc.dsw
 
 import grails.rest.*
 import grails.converters.*
+import grails.plugin.springsecurity.annotation.Secured
 
+@Secured(['ROLE_LOCADORA', 'ROLE_ADMIN', 'ROLE_CLIENTE'])
 class LocacaoController extends RestfulController {
     static responseFormats = ['json', 'xml']
     LocacaoController() {
