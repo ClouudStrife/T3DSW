456e582a7b26e383496ced8e44fd9b9e2fb42324
Gabriel Nardy <gtnardy@gmail.com>
2019-07-02 12:07:53 -0300

    CLIENTE GROV

--- BackEnd/grails-app/domain/br/ufscar/dc/dsw/Cliente.groovy
+++ BackEnd/grails-app/domain/br/ufscar/dc/dsw/Cliente.groovy
@@ -3,25 +3,29 @@ package br.ufscar.dc.dsw
 import grails.rest.*
 
 @Resource(readOnly = false, formats = ['json', 'xml'])
-class Cliente extends User {
+class Cliente {
 	String cpf
 	String nome
 	String email
 	String telefone
+	String username
+	String password
 	String sexo
 	String data_nascimento
-	
+
 	Cliente(String c, String n, String e, String p, String u, String t, String s, String d) {
 		this.cpf = c;
 		this.nome = n;
 		this.email = e;
-		this.password = p;
 		this.username = c; // login por cpf
 		this.telefone = t;
+		this.password = p;
 		this.sexo = s;
 		this.data_nascimento = d;
-		save()
-		UserRole.create(this, Role.get(2), true)
+
+		User us = new User(username: c, password: p);
+		us.save();
+		UserRole.create(us, Role.get(2), true);
 	}
 
 	String toString(){
