6586acc41688fd8770ab222bcefa374d766a7832
Gabriel Nardy <gtnardy@gmail.com>
2019-07-02 11:04:56 -0300

    criado Locadora que extende de User

--- BackEnd/grails-app/domain/br/ufscar/dc/dsw/Locadora.groovy
+++ BackEnd/grails-app/domain/br/ufscar/dc/dsw/Locadora.groovy
@@ -3,14 +3,25 @@ package br.ufscar.dc.dsw
 import grails.rest.*
 
 @Resource(readOnly = false, formats = ['json', 'xml'])
-class Locadora {
+class Locadora extends User {
 	String cnpj
 	String nome
 	String email
-	String senha
+	String password
 	String cidade
 
+	Locadora(String c, String n, String e, String p, String ci) {
+		this.cnpj = c;
+		this.nome = n;
+		this.email = e;
+		this.password = p;
+		this.username = c; //login por cnpj
+		this.cidade = ci;
+		save()
+		UserRole.create(this, Role.get(3), true)
+	}
+
 	String toString(){
-		return '{"cnpj": "' + cnpj + '", "nome": "' + nome + '", "email": "' + email + '", "senha": "' + senha + '", "cidade": "' + cidade + '", "papel": "locadora"}'
+		return '{"cnpj": "' + cnpj + '", "nome": "' + nome + '", "email": "' + email + '", "password": "' + password + '", "cidade": "' + cidade + '"}'
     }	
 }
