f15ca67751ac89d8140d22c5d731a01dc9b394ff
Gabriel Nardy <gtnardy@gmail.com>
2019-07-02 11:04:48 -0300

    criado Cliente que extende de User

--- BackEnd/grails-app/domain/br/ufscar/dc/dsw/Cliente.groovy
+++ BackEnd/grails-app/domain/br/ufscar/dc/dsw/Cliente.groovy
@@ -3,16 +3,28 @@ package br.ufscar.dc.dsw
 import grails.rest.*
 
 @Resource(readOnly = false, formats = ['json', 'xml'])
-class Cliente {
+class Cliente extends User {
 	String cpf
 	String nome
 	String email
-	String senha
 	String telefone
 	String sexo
 	String data_nascimento
+	
+	Cliente(String c, String n, String e, String p, String u, String t, String s, String d) {
+		this.cpf = c;
+		this.nome = n;
+		this.email = e;
+		this.password = p;
+		this.username = c; // login por cpf
+		this.telefone = t;
+		this.sexo = s;
+		this.data_nascimento = d;
+		save()
+		UserRole.create(this, Role.get(2), true)
+	}
 
 	String toString(){
-		return '{"cpf": "' + cpf + '", "nome": "' + nome + '", "email": "' + email + '", "senha": "' + senha + '", "telefone": "' + telefone + '", "sexo": "' + sexo + '", "data_nascimento": "' + data_nascimento + '", "papel": "cliente"}'
+		return '{"cpf": "' + cpf + '", "nome": "' + nome + '", "email": "' + email + '", "telefone": "' + telefone + '", "sexo": "' + sexo + '", "data_nascimento": "' + data_nascimento + '"}'
     }
 }
